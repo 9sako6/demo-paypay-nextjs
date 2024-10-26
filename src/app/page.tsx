f90bb146093bc9d4fd7120a7f77e08734eca237a
Yutaro Taira <9sako6@gmail.com>
2024-10-26 16:38:31 +0900

    QRコードでPayPay支払いできる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const handlePayPayPayment = async () => {
+    const res = await fetch("/api/paypay", {
+      method: "POST",
+    })
+    const json = await res.json()
+
+    window.location.href = json.url
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        PayPay 決済テスト
+        <h1>
+          PayPay 決済テスト
+        </h1>
+
+        <button onClick={handlePayPayPayment}>
+          支払う
+        </button>
       </main>
     </div>
   );
